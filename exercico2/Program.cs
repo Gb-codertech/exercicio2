4823d67ec8e48809270a3c212be4f750cf55af09
Gb-codertech <gbvieira2007@gmail.com>
2024-10-25 12:57:14 +0100

    Add files via upload

---
+++ exercico2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+public class PrimeiroPrograma
+{
+    public static void Main(string[] args)
+    {
+            double raio = 10; // Defina a área do círculo aqui
+
+            double diametro = 2 * raio;
+
+            Console.WriteLine("O diâmetro do círculo é: " + diametro);
+    }
+}
